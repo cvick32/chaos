89d1444451e2cf9d16eb2d50c28f5732605a9f43
Grif Hughes <ghughes@smu.edu>
2017-08-29 17:41:44 -0500

    cleanup

--- include/chaos/gen.h
+++ include/chaos/gen.h
@@ -4,16 +4,16 @@
 #include "../src/attractor.h"
 #include "../src/seed.h"
 
-/* removes initial noise - controls number of initial iterations before numbers are given out */
+/* removes initial noise */
 #define DISCARD 64 
+
 #define ROL(x,n) ((x = ((x << n) | (x >> (32 - n)))))
-#define P(w,x,y,z) ((w ^= x ^ y ^ z))  
 
 struct gen {
         struct attractor as[2];
 };
 
-void gen_init(struct gen *k);
-uint32_t gen32(struct gen *k);
+void gen_init(struct gen *g);
+uint32_t gen32(struct gen *g);
 
 #endif
--- src/gen.c
+++ src/gen.c
@@ -1,58 +1,50 @@
 #include <chaos/gen.h>
 
-void gen_init(struct gen *k)
+void gen_init(struct gen *g)
 {
-        ainit(&k->as[0], gen_seed);
-        ainit(&k->as[1], gen_seed);
+        ainit(&g->as[0], gen_seed);
+        ainit(&g->as[1], gen_seed);
         for(uint8_t i = 0; i < DISCARD - 1; ++i) {
-                anext(&k->as[0]);
-                anext(&k->as[1]);
+                anext(&g->as[0]);
+                anext(&g->as[1]);
         }
 }
 
-static void extract(struct gen *k, uint32_t out[12])
-{
-        out[0] = (uint32_t)(k->as[0].x.p.mant >> 32);
-        out[1] = (uint32_t)(k->as[0].x.p.mant);
-        out[2] = (uint32_t)(k->as[0].y.p.mant >> 32);
-        out[3] = (uint32_t)(k->as[0].y.p.mant);
-        out[4] = (uint32_t)(k->as[0].z.p.mant >> 32);
-        out[5] = (uint32_t)(k->as[0].z.p.mant);
-        out[6] = (uint32_t)(k->as[1].x.p.mant >> 32);
-        out[7] = (uint32_t)(k->as[1].x.p.mant);
-        out[8] = (uint32_t)(k->as[1].y.p.mant >> 32);
-        out[9] = (uint32_t)(k->as[1].y.p.mant);
-        out[10] = (uint32_t)(k->as[1].z.p.mant >> 32);
-        out[11] = (uint32_t)(k->as[1].z.p.mant);
-}
-
-uint32_t gen32(struct gen *k)
+uint32_t gen32(struct gen *g)
 {
         static uint32_t m[12];
-        extract(k, m);
-        anext(&k->as[0]);
-        anext(&k->as[1]);
+        m[0] = (uint32_t)(g->as[0].x.p.mant >> 32);
+        m[1] = (uint32_t)(g->as[0].x.p.mant);
+        m[2] = (uint32_t)(g->as[0].y.p.mant >> 32);
+        m[3] = (uint32_t)(g->as[0].y.p.mant);
+        m[4] = (uint32_t)(g->as[0].z.p.mant >> 32);
+        m[5] = (uint32_t)(g->as[0].z.p.mant);
+        m[6] = (uint32_t)(g->as[1].x.p.mant >> 32);
+        m[7] = (uint32_t)(g->as[1].x.p.mant);
+        m[8] = (uint32_t)(g->as[1].y.p.mant >> 32);
+        m[9] = (uint32_t)(g->as[1].y.p.mant);
+        m[10] = (uint32_t)(g->as[1].z.p.mant >> 32);
+        m[11] = (uint32_t)(g->as[1].z.p.mant);
+        anext(&g->as[0]);
+        anext(&g->as[1]);
 
-        m[0]  += m[1];
-        m[2]  += m[3];
-        m[4]  += m[5];
-        m[6]  += m[7];
-        m[8]  += m[9];
+        m[0] += m[1];
+        m[2] += m[3];
+        m[4] += m[5];
+        m[6] += m[7];
+        m[8] += m[9];
         m[10] += m[11];
-        for(uint8_t i = 0; i < 24; ++i) {
-                m[3] += m[5];
-                m[7] += m[9];
-                m[11] += m[1];
-                ROL(m[0],7);
-                ROL(m[4],13);
-                ROL(m[2],7);
-                ROL(m[6],13);
-                ROL(m[8],7);
-                ROL(m[10],13);
-                P(m[1],m[0],m[4],m[3]);
-                P(m[5],m[2],m[6],m[7]);
-                P(m[9],m[8],m[10],m[11]);
+        for(uint8_t i = 0; i < 18; ++i) {
+                m[11] += m[5];
+                m[3] += m[9];
+                m[7] += m[1];
+                ROL(m[4],7); ROL(m[6],13);
+                ROL(m[8],7); ROL(m[10],13);
+                ROL(m[0],7); ROL(m[2],13);
+                m[5] = m[5] ^ m[8] ^ m[10] ^ m[7];
+                m[9] = m[9] ^ m[0] ^ m[2] ^ m[11];
+                m[1] = m[1] ^ m[4] ^ m[6] ^ m[3];
         }
-        m[1] ^= (m[5] ^ m[9]);
+        m[1] = m[1] ^ m[5] ^ m[9];
         return m[1];
 }
